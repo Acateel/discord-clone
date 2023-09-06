cc1e53ac5ff8bf323168b223e02e5f970b16aca5
Taras Kozodoi <acateely@gmail.com>
2023-09-06 14:39:07 +0300

    add clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div>
       <p className="text-3xl font-bold text-indigo-500">Discord Clone</p>
-      <Button variant='destructive'>Click me</Button>
+      <p>This is a protected route</p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
