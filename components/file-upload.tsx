647e6a1e629c124f23d83b6aac33e8d73943d5e5
Taras Kozodoi <acateely@gmail.com>
2023-09-07 19:56:17 +0300

    add show and remove upload image

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react'
 
+import { X } from 'lucide-react'
+import Image from 'next/image'
+
 import { UploadDropzone } from '@/lib/uploadthing'
 import '@uploadthing/react/styles.css'
 
@@ -10,6 +13,23 @@ interface FileUploadProps {
 }
 
 const FileUpload: FC<FileUploadProps> = ({ endpoint, value, onChange }) => {
+  const fileType = value?.split('.').pop()
+
+  if (value && fileType !== 'pdf') {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange('')}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
