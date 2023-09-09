b3bdf174ecb47adf7268a4d319f9f8565cd7002d
Taras Kozodoi <acateely@gmail.com>
2023-09-09 11:24:23 +0300

    add server sidebar component

---
+++ components/server/server-sidebar.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+interface ServerSidebarProps {}
+
+const ServerSidebar: FC<ServerSidebarProps> = ({}) => {
+  return <div>ServerSidebar</div>
+}
+
+export default ServerSidebar
